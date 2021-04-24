28b5b257c39cf82b99f122527b586bf1d4c9f8b6
“[Shahin415]” <[shah.newaz60@gmail.com]>
2021-04-24 12:05:13 +0600

    updated Lec-10

---
+++ lecture_10_updated/main.go
@@ -0,0 +1,54 @@
+package main
+
+import "fmt"
+
+
+func main () {
+//primitive data types
+//rune,byte=alias
+//int, float32,string, bool
+
+//Composite Data Types Maps //made by primitive mix data in stuck way-this is array
+//array
+//bracket=[]
+//braces/curly braces={}
+//parenthesis=()
+
+//var students [3]string     //array stucture
+
+//fixed length
+//same type data     //if string all string if int all same way
+//0,1,2,13,4 in programming like this count
+//data set or data assign
+
+//students[0]="Shahin"
+//students[1]="Diba"
+//students[2]="Mahi"
+
+//[  ]
+//fmt.Println(students)
+//fmt.Println(len(students))
+//data pull,data retrive, data get
+//fmt.Println(students[2])
+
+//short hand way,string literal
+//in compurter science, literal is a notation 
+//for represengint a fixed value in source code
+
+//students := [3]string{"Shahin","Diba","Mahi"}
+students := [...]string{"Shahin","Diba","Mahi","Mostain","shifa"}  //if more string take
+fmt.Println(students,len(students))
+
+//slice
+//map
+//stuck
+//key = value
+
+
+
+}
+ 
+
+
+
+ 
