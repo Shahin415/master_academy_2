d75a94d59ac2e165e5c5544d3cd0d2a31d809c55
“[Shahin415]” <[shah.newaz60@gmail.com]>
2021-04-24 11:59:54 +0600

    arrary type date lec_10 update

---
+++ New folder (2)/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+
+func main () {
+//primitive data types
+//rune,byte=alias
+//int,float32,string,bool
+
+//Composite Data Types Maps
+//map
+//key = value
+
+//null
+//nil
+x := make(map[string]string)
+x["name"] = "Shahin"
+x["height"] = "5.7"
+x["address"] = "Dhaka"
+
+//delete(x, "height")
+
+fmt.Println(x)
+
+
+}
+ 
+
+
+
+ 
